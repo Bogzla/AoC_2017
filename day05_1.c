e0a79b8c35dfb7d9a64a1dd282dca6859a163a84
Bogzla <bogzla@yahoo.co.uk>
2017-12-07 13:02:44 +0000

    Create day05_1.c

---
+++ day05_1.c
@@ -0,0 +1,38 @@
+// Usage: day05_1 < INPUT.txt
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+const int MAX = 2000;
+
+int main(void)
+{
+  int inst[MAX];
+  int i3 = 0;
+
+  for (int i = 0; i < MAX; i++)
+  {
+    // read in input
+    char s[BUFSIZ];
+    if (scanf("%s", s) == EOF)
+    {
+      break;
+    }
+    else
+    {
+      inst[i] = atoi(s);
+      i3 = i; //get highest element #
+     // printf("You entered: %i\n",inst[i]);
+    }
+  }
+  int i = 0; //current position
+  int i2 = 0; //# steps
+  while (i <= i3)
+  {
+    int i4 = inst[i]; //get instruction
+    inst[i]++; //increment instruction
+    i += i4; // carry out original instruction
+    i2++; //increment count
+  }
+  printf("%i\n", i2);
+}
